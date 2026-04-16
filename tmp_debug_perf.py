4aa621c9ad330b77cdfb8682026765914845512d
Enio Carstens Telles <03002693901@sefin.ro.gov.br>
2026-04-16 11:47:05 -0400

    chore: remove tmp_debug files

--- tmp_debug_perf.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-import importlib
-pm = importlib.import_module('src.utilitarios.perf_monitor')
-print('pm._root_dir():', pm._root_dir())
-print('project paths PROJECT_ROOT:', importlib.import_module('src.utilitarios.project_paths').PROJECT_ROOT)
