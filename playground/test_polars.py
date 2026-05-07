50be408c7dfb37dcfd20a6e5f9159d8c94def149
Enio-Telles <eniotelles@gmail.com>
2026-05-07 13:08:00 -0400

    chore(onda-0): limpa raiz sem regra fiscal

--- playground/test_polars.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-df = pl.DataFrame({"a": ["x", "y", "x"], "b": [1, 2, 3]})
-d = df.partition_by("a", as_dict=True)
-print(d.keys())
-for k, v in d.items():
-    print(f"Key: {k}, Type: {type(k)}")
-    print(v)
--- playground/test_polars2.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-df = pl.DataFrame({"a": ["x", "y", "x"], "b": [1, 2, 3]})
-try:
-    d = df.partition_by("a", as_dict=True)
-    print(d[("x",)])
-except Exception as e:
-    print(e)
